90f69994a372e6ce1eff0ee3df615e80e5ba3a30
sha314 <shahnoor3pl@gmail.com>
2018-10-27 21:25:32 +0600

    syncing with master branch

--- src/convolution/convolution.cpp
+++ src/convolution/convolution.cpp
@@ -8,6 +8,7 @@
 #include <mutex>
 #include "convolution.h"
 #include "binomial.h"
+#include <omp.h>
 
 using namespace std;
 
@@ -478,13 +479,16 @@ vector<vector<double>> convolve_multi_threaded_v2(const vector<vector<double>> &
     return data_out;
 }
 
+
 /*****************************************************
  * Methods of the Convolution class
  */
 
-Convolution::Convolution(size_t n) {
-    initialize(n);
-    _initialized = true;
+Convolution::Convolution(int threads) {
+    _number_of_threads = threads;
+    if(_number_of_threads <= 0 || _number_of_threads > omp_get_max_threads()){
+        _number_of_threads = omp_get_max_threads();
+    }
 }
 
 /**
@@ -492,14 +496,14 @@ Convolution::Convolution(size_t n) {
  */
 void Convolution::initialize(size_t n)  {
     auto t0 = chrono::system_clock::now();
-    this->_number_of_data = n;
-    this->_forward_factor.resize(this->_number_of_data);
-    this->_backward_factor.resize(this->_number_of_data);
+    N = n;
+    _forward_factor.resize(N);
+    _backward_factor.resize(N);
 
-    for (size_t i=0; i < this->_number_of_data; ++i)
+    for (size_t i=0; i < N; ++i)
     {
-        this->_forward_factor[i] = (double) (this->_number_of_data - i + 1) / i;
-        this->_backward_factor[i] = (double) (i + 1) / (this->_number_of_data - i);
+        _forward_factor[i]  = (double) (N - i + 1) / i;
+        _backward_factor[i] = (double) (i + 1) / (N - i);
     }
 
     auto t1 = chrono::system_clock::now();
@@ -513,21 +517,19 @@ void Convolution::initialize(size_t n)  {
  * @return : convolved version of data_in
  */
 vector<double> Convolution::run(vector<double>& data_in) {
-    size_t n = data_in.size();
-    if(!_initialized || n != _number_of_data){
-        // if binomial is not initializd or the number of data does not match
-        initialize(n);
-    }
-    vector<double> data_out(_number_of_data);
+    size_t N = data_in.size();
+    initialize(N);
+
+    vector<double> data_out(N);
     auto t0 = chrono::system_clock::now();
-    long step = _number_of_data / 1000;
-    for (long j=0; j <_number_of_data; ++j)
+    long step = N / 1000;
+    for (long j=0; j <N; ++j) // start from j=1
     {
-        double prob     = (double) j / _number_of_data;
+        double prob     = (double) j / N;
         double factor   = 0;
         double binom    = 0;
         double prev     = 0;
-        double binomNormalization_const = 1;
+        double bn_tot   = 1; // normalization factor
         double sum      = data_in[j];
 
 
@@ -535,10 +537,10 @@ vector<double> Convolution::run(vector<double>& data_in) {
         factor = prob / (1-prob);
         prev   = 1;
 
-        for (long i=j+1; i<_number_of_data; ++i)
+        for (long i=j+1; i<N; ++i)
         {
             binom     = prev * _forward_factor[i] * factor;
-            binomNormalization_const += binom;
+            bn_tot += binom;
             sum      += data_in[i] * binom;
             prev      = binom;
         }
@@ -550,20 +552,21 @@ vector<double> Convolution::run(vector<double>& data_in) {
         for (long i=j-1; i>=0; --i)
         {
             binom     = prev * _backward_factor[i] * factor;
-            binomNormalization_const += binom;
+            bn_tot += binom;
             sum      += data_in[i] * binom;
             prev      = binom;
         }
 
         // normalizing data
-        data_out[j] = sum / binomNormalization_const;
-        if(j % step == 0) {
-            cout << "\33[2K"; // erase the current line
-            cout << '\r'; // return the cursor to the start of the line
-//            cout << "row " << j << " ";
-            cout << "progress " << j * 100 / double(_number_of_data) << " %";
-            std::fflush(stdout);
-        }
+        data_out[j] = sum / bn_tot;
+        cout << bn_tot << endl;
+//        if(j % step == 0) {
+//            cout << "\33[2K"; // erase the current line
+//            cout << '\r'; // return the cursor to the start of the line
+////            cout << "row " << j << " ";
+//            cout << "progress " << j * 100 / double(N) << " %";
+//            std::fflush(stdout);
+//        }
 
     }
     auto t1 = chrono::system_clock::now();
@@ -579,19 +582,17 @@ vector<double> Convolution::run(vector<double>& data_in) {
  * @return : convolved version of data_in
  */
 vector<double> Convolution::run_omp(vector<double>& data_in) {
-    size_t n = data_in.size();
-    if(!_initialized || n != _number_of_data){
-        // if binomial is not initializd or the number of data does not match
-        initialize(n);
-    }
-    vector<double> data_out(_number_of_data);
+    size_t N = data_in.size();
+    initialize(N);
+
+    vector<double> data_out(N);
     auto t0 = chrono::system_clock::now();
-    long step = _number_of_data / 1000;
+    long step = N / 1000;
     // entering parallel region
 #pragma omp parallel for schedule(dynamic)
-    for (long j=0; j <_number_of_data; ++j)
+    for (long j=0; j <N; ++j)
     {
-        double prob     = (double) j / _number_of_data;
+        double prob     = (double) j / N;
         double factor   = 0;
         double binom    = 0;
         double prev     = 0;
@@ -603,7 +604,7 @@ vector<double> Convolution::run_omp(vector<double>& data_in) {
         factor = prob / (1-prob);
         prev   = 1;
 
-        for (long i=j+1; i<_number_of_data; ++i)
+        for (long i=j+1; i<N; ++i)
         {
             binom     = prev * _forward_factor[i] * factor;
             binomNormalization_const += binom;
@@ -629,7 +630,7 @@ vector<double> Convolution::run_omp(vector<double>& data_in) {
             cout << "\33[2K"; // erase the current line
             cout << '\r'; // return the cursor to the start of the line
 //            cout << "row " << j << " ";
-            cout << "progress " << j * 100 / double(_number_of_data) << " %";
+            cout << "progress " << j * 100 / double(N) << " %";
             std::fflush(stdout);
         }
 
@@ -647,21 +648,19 @@ vector<double> Convolution::run_omp(vector<double>& data_in) {
  * @return : convolved version of data_in
  */
 vector<double> Convolution::run_acc(vector<double>& data_in) {
-    size_t n = data_in.size();
-    if(!_initialized || n != _number_of_data){
-        // if binomial is not initializd or the number of data does not match
-        initialize(n);
-    }
-    vector<double> data_out(_number_of_data);
+    size_t N = data_in.size();
+    initialize(N);
+
+    vector<double> data_out(N);
     auto t0 = chrono::system_clock::now();
-    long step = _number_of_data / 1000;
+    long step = N / 1000;
     // entering parallel region
     // take copy of arrays to each loop
 #pragma acc data copy(data_out[0:_number_of_data]) copyin(_forward_factor[0:_number_of_data],_backward_factor[0:_number_of_data],d[0:_number_of_data])
 #pragma acc parallel loop independent
-    for (long j=0; j <_number_of_data; ++j)
+    for (long j=0; j <N; ++j)
     {
-        double prob     = (double) j / _number_of_data;
+        double prob     = (double) j / N;
         double factor   = 0;
         double binom    = 0;
         double prev     = 0;
@@ -673,7 +672,7 @@ vector<double> Convolution::run_acc(vector<double>& data_in) {
         factor = prob / (1-prob);
         prev   = 1;
 
-        for (long i=j+1; i<_number_of_data; ++i)
+        for (long i=j+1; i<N; ++i)
         {
             binom     = prev * _forward_factor[i] * factor;
             binomNormalization_const += binom;
@@ -699,7 +698,7 @@ vector<double> Convolution::run_acc(vector<double>& data_in) {
             cout << "\33[2K"; // erase the current line
             cout << '\r'; // return the cursor to the start of the line
 //            cout << "row " << j << " ";
-            cout << "progress " << j * 100 / double(_number_of_data) << " %";
+            cout << "progress " << j * 100 / double(N) << " %";
             std::fflush(stdout);
         }
 
@@ -717,15 +716,12 @@ vector<double> Convolution::run_acc(vector<double>& data_in) {
  * @return : convolved version of data_in
  */
 vector<double> Convolution::run_pthread(vector<double> &data_in) {
-    size_t n = data_in.size();
-    if(!_initialized || n != _number_of_data){
-        // if binomial is not initializd or the number of data does not match
-        initialize(n);
-    }
-    vector<double> data_out(_number_of_data);
+    size_t N = data_in.size();
+    initialize(N);
+    vector<double> data_out(N);
 
     size_t number_of_threads = std::thread::hardware_concurrency(); // number of threads
-    size_t loop_per_thread = _number_of_data / number_of_threads;
+    size_t loop_per_thread = N / number_of_threads;
     vector<thread> threads(number_of_threads); // holds the threads
     size_t start, stop;
 
@@ -733,7 +729,7 @@ vector<double> Convolution::run_pthread(vector<double> &data_in) {
     for(size_t i{}; i != number_of_threads; ++i){
         start = i*loop_per_thread;
         stop = (i+1) * loop_per_thread;
-         ////each thread finishes independently
+        ////each thread finishes independently
         threads[i] = std::thread(
                 &Convolution::convolution_single_range,
                 this,
@@ -785,7 +781,7 @@ void Convolution::convolution_single_range(
 ) {
     for (long j=row_start; j < row_stop; ++j)
     {
-        double prob     = (double) j / _number_of_data;
+        double prob     = (double) j / N;
         double factor   = 0;
         double binom    = 0;
         double prev     = 0;
@@ -797,7 +793,7 @@ void Convolution::convolution_single_range(
         factor = prob / (1-prob);
         prev   = 1;
 
-        for (long i=j+1; i < _number_of_data; ++i)
+        for (long i=j+1; i < N; ++i)
         {
             binom     = prev * _forward_factor[i] * factor;
             binomNormalization_const += binom;
@@ -847,10 +843,7 @@ std::vector<std::vector<double>> Convolution::run_multi(vector<vector<double>> &
 //    cout << "rows " << n_rows << endl;
 //    cout << "cols " << n_columns << endl;
 
-    if(!_initialized || n_rows != _number_of_data){
-        // if binomial is not initializd or the number of data does not match
-        initialize(n_rows);
-    }
+    initialize(n_rows);
     vector<vector<double>> data_out(n_rows);
 
     size_t step = n_rows / 1000;
@@ -942,10 +935,7 @@ std::vector<std::vector<double>> Convolution::run_multi_omp(vector<vector<double
 //    cout << "rows " << n_rows << endl;
 //    cout << "cols " << n_columns << endl;
 
-    if(!_initialized || n_rows != _number_of_data){
-        // if binomial is not initializd or the number of data does not match
-        initialize(n_rows);
-    }
+    initialize(n_rows);
     vector<vector<double>> data_out(n_rows);
 
 
@@ -953,9 +943,10 @@ std::vector<std::vector<double>> Convolution::run_multi_omp(vector<vector<double
 
     // entering parallel region
     cout << endl;
-    long step = n_rows / 1000;
-#pragma omp parallel for schedule(dynamic)
+    long step = n_rows / 1000 + 1;
+#pragma omp parallel for schedule(dynamic) num_threads(_number_of_threads)
     for (long row=0; row < n_rows; ++row){
+//        cout << "Threads " << omp_get_num_threads() << endl;
         data_out[row].resize(n_columns); // space for columns
         double prob     = (double) row / n_rows;
         double factor   = 0;
@@ -968,7 +959,6 @@ std::vector<std::vector<double>> Convolution::run_multi_omp(vector<vector<double
             sum[k] = data_in[row][k];
         }
 
-//        cout << "line : " << __LINE__ << endl;
 
         // forward iteration part
         factor = prob / (1-prob);
@@ -983,7 +973,6 @@ std::vector<std::vector<double>> Convolution::run_multi_omp(vector<vector<double
             }
             prev      = binom;
         }
-//        cout << "line : " << __LINE__ << endl;
         // backward iteration part
         factor = (1-prob)/prob;
         prev   = 1;
@@ -997,16 +986,13 @@ std::vector<std::vector<double>> Convolution::run_multi_omp(vector<vector<double
             }
             prev      = binom;
         }
-//        cout << "line : " << __LINE__ << endl;
         // normalizing data
         for(size_t j{}; j < n_columns; ++j){
             data_out[row][j] = sum[j] / binomNormalization_const;
-//            cout << "j " << j << endl;
         }
         if(row % step == 0) {
             cout << "\33[2K"; // erase the current line
             cout << '\r'; // return the cursor to the start of the line
-//            cout << "row " << row << " ";
             cout << "progress " << row * 100 / double(n_rows) << " %";
             std::fflush(stdout);
         }
@@ -1016,6 +1002,92 @@ std::vector<std::vector<double>> Convolution::run_multi_omp(vector<vector<double
     _time_elapsed_convolution = chrono::duration<double>(t1 - t0).count();
     cout << endl;
     return data_out;
+}
+
+std::vector<std::vector<double>> Convolution::run_multi_omp_v2(vector<vector<double>> &data_in) {
+    size_t n_columns = data_in[0].size(); // number of columns
+    size_t n_rows = data_in.size(); // number of rows
+
+//    cout << "rows " << n_rows << endl;
+//    cout << "cols " << n_columns << endl;
+
+    initialize(n_rows);
+    vector<vector<double>> data_out(n_rows);
+
+
+    auto t0 = chrono::system_clock::now();
+
+    // entering parallel region
+    cout << endl;
+    long step = n_rows / 1000 + 1;
+#pragma omp parallel for schedule(dynamic) num_threads(_number_of_threads)
+    for (long row=0; row < n_rows; ++row){
+//        cout << "Threads " << omp_get_num_threads() << endl;
+        data_out[row].resize(n_columns); // space for columns
+
+        vector<double> sum;
+        double binomNormalization_const = compute_for_row(data_in, n_columns, n_rows, row, sum);
+
+        // normalizing data
+        for(size_t j{}; j < n_columns; ++j){
+            data_out[row][j] = sum[j] / binomNormalization_const;
+        }
+        if(row % step == 0) {
+            cout << "\33[2K"; // erase the current line
+            cout << '\r'; // return the cursor to the start of the line
+            cout << "progress " << row * 100 / double(n_rows) << " %";
+            std::fflush(stdout);
+        }
+
+    }
+    auto t1 = chrono::system_clock::now();
+    _time_elapsed_convolution = chrono::duration<double>(t1 - t0).count();
+    cout << endl;
+    return data_out;
+}
+
+double Convolution::compute_for_row(
+        const vector<vector<double>> &data_in,
+        size_t n_columns, size_t n_rows,
+        long row, vector<double> &sum) const {
+    double binomNormalization_const = 1;
+    double prob     = (double) row / n_rows;
+    double factor   = 0;
+    double binom    = 0;
+    double prev     = 0;
+    for(size_t k{}; k < n_columns; ++k){
+        sum[k] = data_in[row][k];
+    }
+
+//        cout << "line : " << __LINE__ << endl;
+
+    // forward iteration part
+    factor = prob / (1-prob);
+    prev   = 1;
+
+    for (long i=row+1; i < n_rows; ++i)
+    {
+        binom     = prev * _forward_factor[i] * factor;
+        binomNormalization_const += binom;
+        for(size_t j{}; j < n_columns; ++j){
+            sum[j] += data_in[i][j] * binom;
+        }
+        prev      = binom;
+    }
+//        cout << "line : " << __LINE__ << endl;
+    // backward iteration part
+    factor = (1-prob)/prob;
+    prev   = 1;
+
+    for (long i=row-1; i>=0; --i) {
+        binom     = prev * _backward_factor[i] * factor;
+        binomNormalization_const += binom;
+        for(size_t j{}; j < n_columns; ++j){
+            sum[j] += data_in[i][j] * binom;
+        }
+        prev      = binom;
+    }
+    return binomNormalization_const;
 }
 
 /**
@@ -1041,17 +1113,14 @@ std::vector<std::vector<double>> Convolution::run_multi_pthread(vector<vector<do
 
 //    cout << "rows " << n_rows << endl;
 //    cout << "cols " << n_columns << endl;
+    initialize(n_rows);
 
-    if(!_initialized || n_rows != _number_of_data){
-        // if binomial is not initializd or the number of data does not match
-        initialize(n_rows);
-    }
     vector<vector<double>> data_out(n_rows);
 
     size_t step = n_rows / 1000;
 
     size_t number_of_threads = std::thread::hardware_concurrency(); // number of threads
-    size_t loop_per_thread = _number_of_data / number_of_threads;
+    size_t loop_per_thread = N / number_of_threads;
     vector<thread> threads(number_of_threads); // holds the threads
     size_t start, stop;
 
